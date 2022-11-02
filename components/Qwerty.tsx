2a89e001b10518846526dfd97b24f53a53e03fe2
davis86t <4833583@gmail.com>
2022-11-02 17:14:08 -0500

    Base functionality

--- components/Qwerty.tsx
+++ components/Qwerty.tsx
@@ -1,3 +1,14 @@
 export default function Qwerty() {
-  return <div>qwerty</div>
+  const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+  return (
+    <div>
+      {qwerty.map((row) => (
+        <div className='flex justify-center'>
+          {row.split('').map((key) => (
+            <div className='m-px flex h-10 w-10 items-center justify-center rounded-md bg-gray-400 uppercase'>{key}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,18 @@ export default observer(function Home() {
   const store = useLocalObservable(() => PuzzleStore)
   useEffect(() => {
     store.init()
+    window.addEventListener('keyup', store.handleKeyup)
+
+    return () => {
+      window.removeEventListener('keyup', store.handleKeyup)
+    }
   }, [])
-  return <div className='flex h-screen w-screen flex-col items-center justify-center bg-gray-600'>
+  return (
+  <div className='flex h-screen w-screen flex-col items-center justify-center bg-gray-600'>
     <h1 className='text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-purple-400'>
       Wordle
     </h1>
-    {new Array(6).fill(0).map((_, i) => (
+    {store.guesses.map((_, i) => (
       <Guess
         key={i}
         word={store.word}
@@ -21,7 +27,14 @@ export default observer(function Home() {
         isGuessed={i < store.currentGuess}
       />
     ))}
-    <h1>won/lost</h1>
-    <Qwerty />
+    {store.won && <h1>You won!</h1>}
+    {store.lost && <h1>You lost!</h1>}
+    {(store.won || store.lost) && (
+      <button onClick={store.init}>Play Again</button>
+    )}
+    <Qwerty store={store} />
+    word: {store.word}
+    guesses: {JSON.stringify(store.guesses)}
   </div>
+  )
 })
